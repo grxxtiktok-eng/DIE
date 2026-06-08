1f788caec2a463668f4d8888b8e3b32f398d1781
grxxtiktok-eng <grxxtiktok@gmail.com>
2026-06-08 13:45:48 -0930

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,9 +25,9 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Made with ❤️",
+        name: "discord.gg/die1",
         // Activity type number (0 = Playing).
-        type: 0, 
+        type: 4, 
       },
     ],
   },
